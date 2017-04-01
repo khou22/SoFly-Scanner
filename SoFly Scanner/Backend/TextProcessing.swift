e26fb8288ff22c35da313b19ac60f89025b7cb63
Non <rohanrao1@gmail.com>
2017-04-01 13:32:45 -0400

    Basic Text Functionality

--- SoFly Scanner/Backend/TextProcessing.swift
+++ SoFly Scanner/Backend/TextProcessing.swift
@@ -1,4 +1,4 @@
-//
+﻿//
 //  TextProcessing.swift
 //  SoFly Scanner
 //
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+typealias TaggedToken = (String, String?)
+
+
+// SEMANTIC OPERATIONS - THESE IDENTIFY PARTS OF SPEECH AND AND SEMANTIC RELATIONS
+// OPEN SOURCE NFP
+func tag(text: String, scheme: String) -> [TaggedToken] {
+    let options: NSLinguisticTaggerOptions = .OmitWhitespace | .OmitPunctuation
+    let tagger = NSLinguisticTagger(tagSchemes: NSLinguisticTagger.availableTagSchemesForLanguage("en"),
+        options: Int(options.rawValue))
+    tagger.string = text
+    var tokens: [TaggedToken] = []
+    tagger.enumerateTagsInRange(NSMakeRange(0, count(text)), scheme:scheme, options: options) {tag, tokenRange, _, _ in
+        let token = (text as NSString).substringWithRange(tokenRange)
+        tokens.append((token, tag))
+    }
+    return tokens
+}
+
+func partOfSpeech(text: String) -> [TaggedToken] {
+    return tag(text, NSLinguisticTagSchemeLexicalClass)
+}
+
+func lemmatize(text: String) -> [TaggedToken] {
+    return tag(text, NSLinguisticTagSchemeLemma)
+}
+
+func language(text: String) -> [TaggedToken] {
+    return tag(text, NSLinguisticTagSchemeLanguage)
+}
+
+
+//FIRST PASS AT REMOVING EXTRA SPACES AND PUNCTUATION VOMIT
+func preprocess(text: String) -> String {
+	let myString = text
+	let regex = try! NSRegularExpression(pattern: "\s+", options: NSRegularExpressionOptions.CaseInsensitive)
+	let range = NSMakeRange(0, myString.characters.count)
+	let modString = regex.stringByReplacingMatchesInString(myString, options: [], range: range, withTemplate: "\s")
+	return punctuationElmination(modString)
+}
+
+func punctuationElmination(text: String) -> String {
+	let myString = text
+	let regex = try! NSRegularExpression(pattern: "(.|,|?|!)+", options: NSRegularExpressionOptions.CaseInsensitive)
+	let range = NSMakeRange(0, myString.characters.count)
+	let modString = regex.stringByReplacingMatchesInString(myString, options: [], range: range, withTemplate".")
+}
+
+funct dateFinder(text: String) -> String {
+	let myString = text
+	let regex = try! NSRegularExpression(pattern: "", options: NSRegularExpressionOptions.CaseInsensitive)
+	let range = NSMakeRange(0, myString.characters.count)
+}
