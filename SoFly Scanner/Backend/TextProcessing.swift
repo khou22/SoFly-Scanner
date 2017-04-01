cf7f1e6ee043129e32d8fbcfde2b3b4b0c258b96
Kevin Hou <kevin.ch.hou@gmail.com>
2017-04-01 13:48:15 -0400

    Migrated Swift 2.0 to 3.0

--- SoFly Scanner/Backend/TextProcessing.swift
+++ SoFly Scanner/Backend/TextProcessing.swift
@@ -5,58 +5,55 @@
 //  Created by Kevin Hou on 3/31/17.
 //  Copyright © 2017 Kevin Hou. All rights reserved.
 //
-
 import Foundation
 
 typealias TaggedToken = (String, String?)
 
-
 // SEMANTIC OPERATIONS - THESE IDENTIFY PARTS OF SPEECH AND AND SEMANTIC RELATIONS
 // OPEN SOURCE NFP
 func tag(text: String, scheme: String) -> [TaggedToken] {
-    let options: NSLinguisticTaggerOptions = .OmitWhitespace | .OmitPunctuation
-    let tagger = NSLinguisticTagger(tagSchemes: NSLinguisticTagger.availableTagSchemesForLanguage("en"),
+    let options: NSLinguisticTagger.Options = [.omitWhitespace, .omitPunctuation]
+    let tagger = NSLinguisticTagger(tagSchemes: NSLinguisticTagger.availableTagSchemes(forLanguage: "en"),
         options: Int(options.rawValue))
     tagger.string = text
     var tokens: [TaggedToken] = []
-    tagger.enumerateTagsInRange(NSMakeRange(0, count(text)), scheme:scheme, options: options) {tag, tokenRange, _, _ in
-        let token = (text as NSString).substringWithRange(tokenRange)
+    tagger.enumerateTags(in: NSMakeRange(0, text.characters.count), scheme: scheme, options: options, using: { tag, tokenRange, _, _ in
+        let token = (text as NSString).substring(with: tokenRange)
         tokens.append((token, tag))
-    }
-    return tokens
-}
-
-func partOfSpeech(text: String) -> [TaggedToken] {
-    return tag(text, NSLinguisticTagSchemeLexicalClass)
+    })
+    return tag(text: text, scheme: NSLinguisticTagSchemeLexicalClass)
 }
 
 func lemmatize(text: String) -> [TaggedToken] {
-    return tag(text, NSLinguisticTagSchemeLemma)
+    return tag(text: text, scheme: NSLinguisticTagSchemeLemma)
 }
 
 func language(text: String) -> [TaggedToken] {
-    return tag(text, NSLinguisticTagSchemeLanguage)
+    return tag(text: text, scheme: NSLinguisticTagSchemeLanguage)
 }
 
 
-//FIRST PASS AT REMOVING EXTRA SPACES AND PUNCTUATION VOMIT
+// FIRST PASS AT REMOVING EXTRA SPACES AND PUNCTUATION VOMIT
 func preprocess(text: String) -> String {
-	let myString = text
-	let regex = try! NSRegularExpression(pattern: "\s+", options: NSRegularExpressionOptions.CaseInsensitive)
-	let range = NSMakeRange(0, myString.characters.count)
-	let modString = regex.stringByReplacingMatchesInString(myString, options: [], range: range, withTemplate: "\s")
-	return punctuationElmination(modString)
+    let myString = text
+    let regex: NSRegularExpression = try! NSRegularExpression(pattern: "\\s+", options: .caseInsensitive)
+    let range: NSRange = NSMakeRange(0, myString.characters.count)
+    let modString = regex.stringByReplacingMatches(in: myString, options: [], range: range, withTemplate: "\\s")
+    
+    return punctuationElmination(text: modString)
 }
 
 func punctuationElmination(text: String) -> String {
 	let myString = text
-	let regex = try! NSRegularExpression(pattern: "(.|,|?|!)+", options: NSRegularExpressionOptions.CaseInsensitive)
-	let range = NSMakeRange(0, myString.characters.count)
-	let modString = regex.stringByReplacingMatchesInString(myString, options: [], range: range, withTemplate".")
+    let regex: NSRegularExpression = try! NSRegularExpression(pattern: "(.|,|?|!)+", options: .caseInsensitive)
+    let range: NSRange = NSMakeRange(0, myString.characters.count)
+    let modString = regex.stringByReplacingMatches(in: myString, options: [], range: range, withTemplate: ".")
+    
+    return modString
 }
 
-funct dateFinder(text: String) -> String {
-	let myString = text
-	let regex = try! NSRegularExpression(pattern: "", options: NSRegularExpressionOptions.CaseInsensitive)
-	let range = NSMakeRange(0, myString.characters.count)
+func dateFinder(text: String) -> String {
+    let myString = text
+    
+    return myString
 }
