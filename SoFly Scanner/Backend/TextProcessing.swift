0ddb57c05e3b26271816dbe124b0ecaa343368a3
Kevin Hou <kevin.ch.hou@gmail.com>
2017-04-01 17:48:23 -0400

    Imported Rohan's NLP code

--- SoFly Scanner/Backend/TextProcessing.swift
+++ SoFly Scanner/Backend/TextProcessing.swift
@@ -55,26 +55,67 @@ class NaturalLangProcessing {
     }
 
     static func Year(text: String) -> String {
-        let myString: NSString = text as NSString
+        let myString = text as NSString
+        let regex1: NSRegularExpression = try! NSRegularExpression(pattern: "201[0-9]", options: .caseInsensitive)
+        let regex2: NSRegularExpression = try! NSRegularExpression(pattern: "1[0-9]", options: .caseInsensitive)
         
-        // Regexes
-        let regex1: NSRegularExpression = try! NSRegularExpression(pattern: "201.", options: .caseInsensitive)
-        let regex2: NSRegularExpression = try! NSRegularExpression(pattern: "1.", options: .caseInsensitive)
-
+        
         let range: NSRange = NSMakeRange(0, myString.length)
         let modString1 = regex1.matches(in: myString as String, options:[], range:range)
-		let modString2 = regex2.matches(in: myString as String, options:[], range:range)
+        let modString2 = regex2.matches(in: myString as String, options:[], range:range)
         
-//		let f: [String] = modString1.map{
-//            myString.substring($0.range)
-//        }
-//		let l: [String] = modString2.map{myString.substring($0.range)}
-//        var temp: String
-//		if (!f.isEmpty) {
-//			return f[0]
-//		}
-//		return l[0]
+        let f: [String] = modString1.map{
+            myString.substring(with: $0.range)
+        }
+        let l: [String] = modString2.map{
+            myString.substring(with: $0.range)
+        }
         
-        return ""
+        if (!f.isEmpty) {
+            return f[0]
+        }
+        return l[0]
+    }
+    
+    static func Month(text: String) -> String {
+        let myString = text as NSString
+        let regexFullMonths: NSRegularExpression = try! NSRegularExpression(pattern: "(January |February |March |April |May |June |July |August |September |October |November |December )", options: .caseInsensitive)
+        let regexAbbrevs: NSRegularExpression = try! NSRegularExpression(pattern: "(Jan |Feb |Mar |Apr |May |Jun |Jul |Aug |Sept |Sep |Oct |Nov |Dec |Jan\\. |Feb\\. |Mar\\. |Apr\\. |May\\. |Jun\\. |Jul\\. |Aug\\. |Sept\\. |Sep\\. |Oct\\. |Nov\\. |Dec\\. )", options: .caseInsensitive)
+        let regexNumeric: NSRegularExpression = try! NSRegularExpression(pattern: "([1-9]|0[1-9]|1[0-2])(\\.|-| |\\\\)", options: .caseInsensitive)
+        let range: NSRange = NSMakeRange(0, myString.length)
+        let modString1 = regexFullMonths.matches(in: myString as String, options:[], range:range)
+        let modString2 = regexAbbrevs.matches(in: myString as String, options:[], range:range)
+        let modString3 = regexNumeric.matches(in: myString as String, options:[], range:range)
+        let f: [String] = modString1.map{myString.substring(with: $0.range)}
+        let l: [String] = modString2.map{myString.substring(with: $0.range)}
+        let m: [String] = modString3.map{myString.substring(with: $0.range)}
+        
+        if (!f.isEmpty) {
+            return f[0]
+        }
+        else if(!l.isEmpty) {
+            return l[0]
+        }
+        else {
+            return m[0]
+        }
+    }
+    
+    static func Year(text: String) -> [String] {
+        let myString = text as NSString
+        let regex1: NSRegularExpression = try! NSRegularExpression(pattern: "0[1-9]|[1-31](\\.|-|\\\\| )", options: .caseInsensitive)
+        let range: NSRange = NSMakeRange(0, myString.length)
+        let modString1 = regex1.matches(in: myString as String, options:[], range:range)
+        let f: [String] = modString1.map{
+            myString.substring(with: $0.range)
+        }
+        return f
+    }
+    
+    static func Time(text: String) -> [String] {
+        let regexColon: NSRegularExpression = try! NSRegularExpression(pattern: "(0[1-9]||[0-9]|1[0-2]):(0[0-9]|[10-59])", options: .caseInsensitive)
+        let regexNumberAMPM: NSRegularExpression = try! NSRegularExpression(pattern: "[1-12](am|pm)(\\\\|.|-| )", options: .caseInsensitive)
+        let regexAMPM: NSRegularExpression = try! NSRegularExpression(pattern: "", options: .caseInsensitive)
+        return []
     }
 }
