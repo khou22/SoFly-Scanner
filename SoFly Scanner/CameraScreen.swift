1b9f309cd6228c2457d8ee1c03bfd65f8d8a5339
Kevin Hou <kevin.ch.hou@gmail.com>
2017-04-01 13:58:48 -0400

    Added in live camera preview

--- SoFly Scanner/CameraScreen.swift
+++ SoFly Scanner/CameraScreen.swift
@@ -8,22 +8,65 @@
 
 import Foundation
 import UIKit
+import AVFoundation // Camera control
 
 class CameraScreen: UIViewController {
     
-    @IBOutlet weak var testImageView: UIImageView!
+    // Live camera view
+    @IBOutlet weak var cameraPreview: UIView!
     
-    override func viewDidLoad() {
-        print("Camera screen loaded")
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCapturePhotoOutput()
+    var sessionOutputSetting = AVCapturePhotoSettings(format: [AVVideoCodecKey:AVVideoCodecJPEG])
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let deviceDiscoverySession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInDuoCamera, .builtInTelephotoCamera, .builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .unspecified) // Build list of all devices that satisfy this requirement
+        
+        for device in (deviceDiscoverySession?.devices)! { // Cycle through all requirements
+            if (device.position == AVCaptureDevicePosition.back) { // Use back camera
+                do {
+                    let input = try AVCaptureDeviceInput(device: device) // Try the current input
+                    if (captureSession.canAddInput(input)) { // If valid
+                        captureSession.addInput(input) // Add input to session
+                        
+                        if (captureSession.canAddOutput(sessionOutput)) { // If can also take pictures
+                            captureSession.addOutput(sessionOutput) // Add to output
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = .portrait // Portrait orientation
+                            
+                            self.cameraPreview.layer.addSublayer(previewLayer) // Add the preview
+                            
+                            captureSession.startRunning() // Start the camera session
+                            
+                            break // Break out of loop
+                        }
+                    }
+                }
+                catch {
+                    print("exception!")
+                }
+            }
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        if let testImg = UIImage(named: Images.testPosterHPF2016) {
-            testImageView.image = ImageProcessing.scaleImage(image: testImg, maxDimension: 640)
+        if let testImg = UIImage(named: Images.testPosterHackPrinceton) {
             print("Testing image")
             let str = ImageProcessing.performImageRecognition(image: testImg)
             print(str)
+            
+            let preprocessed: String = NaturalLangProcessing.preprocess(text: str)
+            print(preprocessed)
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.previewLayer.frame = cameraPreview.bounds // Set bounds
     }
     
 }
